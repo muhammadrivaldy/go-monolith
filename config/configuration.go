1ed93a24a27e1613fdc8f1bf3dae69b26dcd1885
Rivaldy <muhammadrivaldy16@gmail.com>
2023-03-27 05:52:07 +0700

    Update config file

--- config/configuration.go
+++ config/configuration.go
@@ -2,32 +2,33 @@ package config
 
 // Configuration is a object configuration
 type Configuration struct {
-	Port    int    `json:"port" env:"PORT"`
-	JWTKey  string `json:"jwt-key" env:"JWT_KEY"`
-	URLUmkm struct {
-		BaseURL               string `json:"base_url" env:"BASE_URL"`
-		ConfirmRegister       string `json:"confirm_register" env:"CONFIRM_REGISTER_URL"`
-		ConfirmForgotPassword string `json:"confirm_forgot_password" env:"CONFIRM_FORGOT_PASSWORD_URL"`
-	} `json:"url_umkm"`
+	Port     int    `json:"port" env:"PORT"`
+	JWTKey   string `json:"jwt-key" env:"JWT_KEY"`
 	Database struct {
-		Address    string `json:"address" env:"DB_ADDRESS"`
 		User       string `json:"user" env:"DB_USER"`
 		Password   string `json:"password" env:"DB_PASSWORD"`
 		Parameters string `json:"parameters" env:"DB_PARAMETERS"`
 		Schema     struct {
-			Security string `json:"security" env:"DB_SCHEMA_SECURITY"`
+			Security struct {
+				Address       string `json:"address" env:"DB_ADDRESS_SECURITY"`
+				Database      string `json:"database" env:"DB_SCHEMA_SECURITY"`
+				MigrationPath string `json:"migration_path" env:"DB_MIGRATION_PATH_SECURITY"`
+			} `json:"security"`
+			Users struct {
+				Address       string `json:"address" env:"DB_ADDRESS_USERS"`
+				Database      string `json:"database" env:"DB_SCHEMA_USERS"`
+				MigrationPath string `json:"migration_path" env:"DB_MIGRATION_PATH_USERS"`
+			} `json:"users"`
 		} `json:"schema"`
 	} `json:"database"`
-	EmailSystem struct {
-		Host     string `json:"host" env:"EMAIL_HOST"`
-		Email    string `json:"email" env:"EMAIL_ADDRESS"`
-		Password string `json:"password" env:"EMAIL_PASSWORD"`
-		Port     int    `json:"port" env:"EMAIL_PORT"`
-	} `json:"email_system"`
+	Redis struct {
+		Address  string `json:"address" env:"REDIS_ADDRESS"`
+		Password string `json:"password" env:"REDIS_PASSWORD"`
+	} `json:"redis"`
 	ThirdParty struct {
 		Telegram struct {
 			Token  string `json:"token" env:"TELEGRAM_TOKEN"`
-			ChatID int64  `json:"chat_id" env:"TELEGRAM_CHAT_ID"`
+			ChatId int64  `json:"chat_id" env:"TELEGRAM_CHAT_ID"`
 		} `json:"telegram"`
 	} `json:"third_party"`
 }
