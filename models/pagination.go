db98ca6dbb3fc14fc79796489ec7158530c04e86
Rivaldy <muhammadrivaldy16@gmail.com>
2023-03-27 05:28:24 +0700

    Update the pagination model

--- models/pagination.go
+++ models/pagination.go
@@ -1,16 +1,46 @@
 package models
 
+import (
+	"math"
+)
+
 type (
 	// Pagination is a object for pagination query
 	Pagination struct {
-		Limit  int `json:"limit" required:"min=min=1"`
-		Offset int `json:"page" required:"min=min=0"`
+		Page  int `validate:"required,min=1"`
+		Limit int `validate:"required,min=1"`
 	}
 
 	// PaginationResponse ..
 	PaginationResponse struct {
-		Limit  int   `json:"limit"`
-		Offset int   `json:"page"`
-		Total  int64 `json:"total"`
+		Page      int `json:"page"`
+		Limit     int `json:"limit"`
+		TotalPage int `json:"total_page"`
+		TotalData int `json:"total_data"`
 	}
 )
+
+func (p Pagination) GetOffset() int {
+
+	if p.Page <= 1 {
+		return 0
+	}
+
+	page := p.Page - 1
+
+	return page * p.Limit
+
+}
+
+func (p *PaginationResponse) SetTotalPage() {
+
+	if p.TotalData <= p.Limit {
+		p.TotalPage = 1
+		return
+	}
+
+	totalPage := float64(p.TotalData) / float64(p.Limit)
+
+	p.TotalPage = int(math.Ceil(totalPage))
+
+}
